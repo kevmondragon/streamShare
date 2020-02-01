ed4b431a30e5f245ab4fac58b4b9a3e42e99a4bd
Matias Rodriguez <NickRodriguez@Matiass-MacBook-Pro.local>
2020-02-01 16:27:32 -0500

    Added react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useFunctions, FirebaseAppProvider, useFirebaseApp } from 'reactfire'
+import 'firebase/functions'
+
+
 
 function App() {
+
+
+  const helloWorldFunction = useFirebaseApp().functions().httpsCallable('helloWorld')
+
+  helloWorldFunction()
+  .then(result => {
+    console.log(result)
+  })
+  .catch(e => {
+    console.log(e)
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload. aifdubsioubfriugb
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,8 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { FirebaseAppProvider, SuspenseWithPerf } from 'reactfire'
+import 'firebase/performance'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const firebaseConfig = {
+    apiKey: "AIzaSyBkw-TlISFpDhrRn03-sc_B4xRWWoG0cOk",
+    authDomain: "stream-share-react.firebaseapp.com",
+    databaseURL: "https://stream-share-react.firebaseio.com",
+    projectId: "stream-share-react",
+    storageBucket: "stream-share-react.appspot.com",
+    messagingSenderId: "305783609456",
+    appId: "1:305783609456:web:fc34c53a2c8d8889a97cb5"
+  };
+
+ReactDOM.render(
+    <FirebaseAppProvider firebaseConfig={firebaseConfig} initPerformance>
+        <SuspenseWithPerf fallback={'Laoding...'} traceId={'loader'}>
+        <App />
+        </SuspenseWithPerf>
+    </FirebaseAppProvider>, 
+document.getElementById('root'))
+;
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
